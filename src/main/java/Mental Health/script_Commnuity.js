28636aef4cbe158665b7902d5c742c9967fa44a3
40381868 <40381868@qub.ac.uk>
2023-03-22 13:34:43 +0000

    Improvement

--- src/main/java/Mental Health/script_Commnuity.js
+++ src/main/java/Mental Health/script_Commnuity.js
@@ -1,64 +1,47 @@
-// 弹出 帖子输入的框---【去掉默认隐藏为显示，设置display = "block";】
-function post(){
-    var inp = document.getElementsByClassName("post");
-    inp[0].style.display = "block";
-}	 
+var box = document.querySelector(".box")
+console.log(box);
+
+var post = document.querySelector(".post");
+var sample = document.querySelector(".sample")
+
+
+post.onclick=function(){
+    var sr = document.querySelector(".sr");
+    var sct = document.querySelector(".sct");
+    var tx = Math.floor(Math.random()*4)+1;
+    document.getElementById('content').value = "";
+
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var date1 = date.getDate();
+    var day = date.getDay();
+    var hours = date.getHours();
+    var min = date.getMinutes();
+    var second = date.getSeconds();
+
+    if(hours < 10) {
+        hours = "0" + hours;
+    }
+    if(min < 10) {
+        min = "0" + min;
+    }
+    if(second < 10) {
+        second = "0" + second;
+    }
     
-// 点击发布
-function postSuccess(){
-    //隐藏发布框
-    var inp = document.getElementsByClassName("post");
-    inp[0].style.display = "none";   
-    
-    // 创建li标签 		【并把li标签添加到ul列表下的首个子标签的位置】
-    var newli=document.createElement("li");
-    var ul = document.getElementById("postul");
-    ul.insertBefore(newli,ul.firstChild);//【保证li当前添加的li标签始终在最上面】
 
-    // 创建div标签 		并往li中添加div标签 
-    var newdiv=document.createElement("div");
-    newli.appendChild(newdiv);
-
-    // 往div中添加图片 设置当前图片的路径
-    var img=document.createElement("img");
-    newdiv.appendChild(img);
-    //随机取1-4的随机数
-    var num = Math.floor(Math.random()*4+1);  
-    //设置路径---通过setAttribute设置属性
-    img.setAttribute("src","images/tou0"+num+".jpg");  
-
-    // 在li中添加h1标签
-    var h1=document.createElement("h1");
-    newli.appendChild(h1);
-    // 获取标题输入框中的内容
-    var biao =  document.getElementById("title");
-    h1.innerText=biao.value; //注意：因为input是单边标签，所以用.value获取
-    
-    // 在li中添加p标签 
-    var newp=document.createElement("p");
-    newli.appendChild(newp);
-
-    // 获取所属板块中的内容
-    var ban = document.getElementById("sec");
-    // 把所选板块的信息写在p标签中
-    newp.innerText = "模块信息："+ban.value;
-
-    // 在p标签中添加span标签
-    var span=document.createElement("span");
-    newp.appendChild(span);
-
-    // 给span标签设置外边框边距、距离标签100px
-    span.style.marginLeft="100px";
-    // 把日期写在span标签中
-    span.innerText="发布时间 :  "+getTime();
+    sample.innerHTML = `<div class="line">
+        <div>
+            <h3>${sr.value}</h3>
+            Topic：<span class="plate">${sct.value}</span>
+            Time：<span class="time">${year}-${month}-${date1}&ensp;${hours}:${min}</span>
+        </div>
+    </div>`+ sample.innerHTML
+    sct.value = "Please select the topic";
+    sr.value = "";
 }
 
-function getTime(){
-     // 1、获取当前的日期
-     var date = new Date();
-    var m = (date.getMonth()+1)>9?(date.getMonth()+1):"0"+(date.getMonth()+1);
-    var min = (date.getMinutes())>9?(date.getMinutes()):"0"+(date.getMinutes());
-    var se = (date.getSeconds())>9?(date.getSeconds()):"0"+(date.getSeconds);
-    var cl = date.getFullYear()+"年"+m+"月"+date.getDate()+"日\t"+date.getHours()+"时"+min+"分"+date.getSeconds()+"秒";
-    return cl;
-}
+
+
+
--- src/main/java/Mental Health/script_Reminder.js
+++ src/main/java/Mental Health/script_Reminder.js
@@ -13,14 +13,11 @@ var my_year = my_date.getFullYear();
 var my_month = my_date.getMonth();
 var my_day = my_date.getDate();
 
-//获取某年某月第一天是星期几
 function dayStart(month, year) {
 	var tmpDate = new Date(year, month, 1);
 	return (tmpDate.getDay());
 }
 
-
-//计算某年是不是闰年，通过求年份除以4的余数即可
 function daysMonth(month, year) {
 	var tmp = year % 4;
 	if (tmp == 0) {
@@ -33,27 +30,27 @@ function daysMonth(month, year) {
 
 function refreshDate(){
 	var str = "";
-	var totalDay = daysMonth(my_month, my_year); //获取该月总天数
-	var firstDay = dayStart(my_month, my_year); //获取该月第一天是星期几
+	var totalDay = daysMonth(my_month, my_year);
+	var firstDay = dayStart(my_month, my_year);
 	var myclass;
 	for(var i=1; i<firstDay; i++){ 
-		str += "<li></li>"; //为起始日之前的日期创建空白节点
+		str += "<li></li>"; 
 	}
 	for(var i=1; i<=totalDay; i++){
 		if((i<my_day && my_year==my_date.getFullYear() && my_month==my_date.getMonth()) || my_year<my_date.getFullYear() || ( my_year==my_date.getFullYear() && my_month<my_date.getMonth())){ 
-			myclass = " class='lightgrey'"; //当该日期在今天之前时，以浅灰色字体显示
+			myclass = " class='lightgrey'";
 		}else if (i==my_day && my_year==my_date.getFullYear() && my_month==my_date.getMonth()){
-			myclass = " class='green greenbox'"; //当天日期以绿色背景突出显示
+			myclass = " class='green greenbox'";
 		}else{
-			myclass = " class='darkgrey'"; //当该日期在今天之后时，以深灰字体显示
+			myclass = " class='darkgrey'";
 		}
-		str += "<li"+myclass+">"+i+"</li>"; //创建日期节点
+		str += "<li"+myclass+">"+i+"</li>";
 	}
-	holder.innerHTML = str; //设置日期显示
-	ctitle.innerHTML = month_name[my_month]; //设置英文月份显示
-	cyear.innerHTML = my_year; //设置年份显示
+	holder.innerHTML = str;
+	ctitle.innerHTML = month_name[my_month];
+	cyear.innerHTML = my_year;
 }
-refreshDate(); //执行该函数
+refreshDate();
 
 
 prev.onclick = function(e){
